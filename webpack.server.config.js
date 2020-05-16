357fbf9d00174e218c635934df98c5abf4e91ba1
Brandon Morrissey <bmorrissey95@gmail.com>
2020-05-16 16:27:31 +1000

    Fixed webpack config

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -24,7 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: [ nodeModules,  ],
+        exclude: [ nodeModules, client ],
         use: {
           loader: 'babel-loader'
         }
